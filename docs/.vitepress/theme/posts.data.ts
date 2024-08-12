56d53cd0519be421b3266140040ec3d82565b114
r2fo <50496756+r2fo@users.noreply.github.com>
2024-08-12 19:27:46 +0200

    custom config for posts loader + css change

--- docs/.vitepress/theme/posts.data.ts
+++ docs/.vitepress/theme/posts.data.ts
@@ -11,7 +11,7 @@ interface Post {
 declare const data: Post[];
 export { data };
 
-export default createContentLoader("demo/blog/posts/*.md", {
+export default createContentLoader("/posts/*.md", {
   excerpt: true,
   transform(raw): Post[] {
     return raw
